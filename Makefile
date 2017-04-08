dc3ebff711a24a957d6f3477d70cd3431f6c8426
h2016107 <h2016107@pilani.bits-pilani.ac.in>
2017-04-08 12:38:45 -0700

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+obj-m := bram.o
+bram-y := ram_block.o ram_device.o partition.o
+
+
---
+++ ram_block.c
@@ -0,0 +1,237 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/version.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/spinlock.h>
+#include <linux/genhd.h> //  basic block driver framework
+#include <linux/blkdev.h> // struct block_device_operations
+#include <linux/hdreg.h> // struct hd_geometry
+#include <linux/errno.h>
+
+#include "ram_device.h"
+
+#define RB_FIRST_MINOR 0
+#define RB_MINOR_CNT 16
+
+static u_int rb_major = 0;
+
+/* 
+  The internal structure representation of my Device
+ */
+static struct rb_device
+{
+	/* Size is the size of the device in sectors */
+	unsigned int size;
+	/*  exclusive access to my request queue */
+	spinlock_t lock;
+	/* my request queue */
+	struct request_queue *rb_queue;
+	/* This is how kernel represent an individual disk device */
+	struct gendisk *rb_disk;
+} rb_dev;
+
+static int rb_open(struct block_device *bdev, fmode_t mode)
+{
+	unsigned unit = iminor(bdev->bd_inode);
+
+	printk(KERN_INFO "The Device is opened\n");
+	printk(KERN_INFO "Inode number is %d\n", unit);
+
+	if (unit > RB_MINOR_CNT)
+		return -ENODEV;
+	return 0;
+}
+
+static void rb_close(struct gendisk *disk, fmode_t mode)
+{
+	printk(KERN_INFO "Device is closed\n");
+}
+
+static int rb_getgeo(struct block_device *bdev, struct hd_geometry *geo)
+{
+	geo->heads = 1;
+	geo->cylinders = 32;
+	geo->sectors = 32;
+	geo->start = 0;
+	return 0;
+}
+
+/* 
+  Actual Data transfer
+ */
+static int rb_transfer(struct request *req)
+{
+	
+
+	int dir = rq_data_dir(req);
+	sector_t start_sector = blk_rq_pos(req);
+	unsigned int sector_cnt = blk_rq_sectors(req);
+
+#define BV_PAGE(bv) ((bv).bv_page)
+#define BV_OFFSET(bv) ((bv).bv_offset)
+#define BV_LEN(bv) ((bv).bv_len)
+	struct bio_vec bv;
+
+	struct req_iterator iter;
+
+	sector_t sector_offset;
+	unsigned int sectors;
+	u8 *buffer;
+
+	int ret = 0;
+
+	
+
+	sector_offset = 0;
+	rq_for_each_segment(bv, req, iter)
+	{
+		buffer = page_address(BV_PAGE(bv)) + BV_OFFSET(bv);
+		if (BV_LEN(bv) % RB_SECTOR_SIZE != 0)
+		{
+			printk(KERN_ERR "Should never happen: "
+				"bio size (%d) is not a multiple of RB_SECTOR_SIZE (%d).\n"
+				"This can lead to data truncation.\n",
+				BV_LEN(bv), RB_SECTOR_SIZE);
+			ret = -EIO;
+		}
+		sectors = BV_LEN(bv) / RB_SECTOR_SIZE;
+		printk(KERN_DEBUG "Start Sector: %llu, Sector Offset: %llu; Buffer: %p; Length: %u sectors\n",
+			(unsigned long long)(start_sector), (unsigned long long)(sector_offset), buffer, sectors);
+		if (dir == WRITE) /* Write to  device */
+		{
+			ramdevice_write(start_sector + sector_offset, buffer, sectors);
+		}
+		else /* Read from device */
+		{
+			ramdevice_read(start_sector + sector_offset, buffer, sectors);
+		}
+		sector_offset += sectors;
+	}
+	if (sector_offset != sector_cnt)
+	{
+		printk(KERN_ERR "bio info doesn't match with the request info");
+		ret = -EIO;
+	}
+
+	return ret;
+}
+	
+/*
+ * Represents a block I/O request for us to execute
+ */
+static void rb_request(struct request_queue *q)
+{
+	struct request *req;
+	int ret;
+
+	/* Gets the current request from the dispatch queue */
+	while ((req = blk_fetch_request(q)) != NULL)
+	{
+
+		ret = rb_transfer(req);
+		__blk_end_request_all(req, ret);
+		
+	}
+}
+
+/* 
+  below are the file operations that performed on the ram block device
+ */
+static struct block_device_operations rb_fops =
+{
+	.owner = THIS_MODULE,
+	.open = rb_open,
+	.release = rb_close,
+	.getgeo = rb_getgeo,
+};
+	
+/* 
+  Below is registration and initialization section of the ram block device
+  driver
+ */
+static int __init rb_init(void)
+{
+	int ret;
+
+	/* Set up my RAM Device */
+	if ((ret = ramdevice_init()) < 0)
+	{
+		return ret;
+	}
+	rb_dev.size = ret;
+
+	/* Get Registered */
+	rb_major = register_blkdev(rb_major, "rb");
+	if (rb_major <= 0)
+	{
+		printk(KERN_ERR "Unable to get Major Number\n");
+		ramdevice_cleanup();
+		return -EBUSY;
+	}
+
+	/* Get a request queue */
+	spin_lock_init(&rb_dev.lock);
+	rb_dev.rb_queue = blk_init_queue(rb_request, &rb_dev.lock);
+	if (rb_dev.rb_queue == NULL)
+	{
+		printk(KERN_ERR "blk_init_queue failure\n");
+		unregister_blkdev(rb_major, "rb");
+		ramdevice_cleanup();
+		return -ENOMEM;
+	}
+	
+	
+	rb_dev.rb_disk = alloc_disk(RB_MINOR_CNT);
+	if (!rb_dev.rb_disk)
+	{
+		printk(KERN_ERR "alloc_disk failure\n");
+		blk_cleanup_queue(rb_dev.rb_queue);
+		unregister_blkdev(rb_major, "rb");
+		ramdevice_cleanup();
+		return -ENOMEM;
+	}
+
+ 	/* Setting the major number */
+	rb_dev.rb_disk->major = rb_major;
+  	/* Setting the first mior number */
+	rb_dev.rb_disk->first_minor = RB_FIRST_MINOR;
+ 	/* Initializing the device operations */
+	rb_dev.rb_disk->fops = &rb_fops;
+ 	/* Driver-specific own internal data */
+	rb_dev.rb_disk->private_data = &rb_dev;
+	rb_dev.rb_disk->queue = rb_dev.rb_queue;
+	
+	sprintf(rb_dev.rb_disk->disk_name, "rb");
+	/* Setting the capacity of the device in its gendisk structure */
+	set_capacity(rb_dev.rb_disk, rb_dev.size);
+
+	/* Adding the disk to the system */
+	add_disk(rb_dev.rb_disk);
+	/* Now the disk is "live" */
+	printk(KERN_INFO "Ram Block driver initialised (%d sectors; %d bytes)\n",
+		rb_dev.size, rb_dev.size * RB_SECTOR_SIZE);
+
+	return 0;
+}
+/*
+ * This is the unregistration and uninitialization section of the ram block
+ * device driver
+ */
+static void __exit rb_cleanup(void)
+{
+	del_gendisk(rb_dev.rb_disk);
+	put_disk(rb_dev.rb_disk);
+	blk_cleanup_queue(rb_dev.rb_queue);
+	unregister_blkdev(rb_major, "rb");
+	ramdevice_cleanup();
+}
+
+module_init(rb_init);
+module_exit(rb_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("praveen hooda");
+MODULE_DESCRIPTION("disc on ram");
+MODULE_ALIAS_BLOCKDEV_MAJOR(rb_major);
